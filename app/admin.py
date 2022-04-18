7d9c875d88591ae2dafae034f92423a94375932a
Tony K <82545368+Nsiem@users.noreply.github.com>
2022-04-18 18:41:55 -0400

    Created all models for app, registered them and plugged app into settings.py, DID not create methods, nor tables for models

--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register()#Model to register in the Admin site)
+app_models = [User, Deck, Card, cardLedger]
+
+admin.site.register(app_models)#Model to register in the Admin site)
