19511aa570132605117c600f0f182cf5e3305759
ide-k <siddharthkhanna2k@gmail.com>
2022-04-24 21:03:24 -0400

    add user auth and create login/register pages
    /auth has a dictionary update sequence error (don't understand)
    /login and /register, are returning None instead of an httprequest

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='index'), path('', views.about, name='about'), path('', views.Deck_Page, name='deck_page'),
+    path('', views.homepage, name='index'), 
+    path('about', views.about, name='about'), 
+    path('deckpage', views.Deck_Page, name='deck_page'),
+    path('auth', views.auth_view, 'auth'),
+    path('register', views.registration_view, name='register'),
+    path('login', views.login_view, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -34,3 +34,77 @@ def Deck_Page(request):
     template_name = "app/homepage.html"
 
     return render(request, template_name, context)
+
+# auth main view
+def auth_view(request):
+    page_title = "Login"
+    registration_form = RegistrationForm()
+    
+    context = {
+        'page_title': page_title,
+        'registration_form': registration_form,
+    }
+    
+    template_name = 'app/auth.html'
+    
+    return render(request, template_name, context)
+
+# registration main view
+def registration_view(request):
+    page_title = "Register"
+    
+    context = {
+        'page_title': page_title,
+    }
+    
+    template_name = 'app/registration.html'
+
+# login main view
+def login_view(request):
+    page_title = "Login"
+    
+    context = {
+        'page_title': page_title,
+    }
+    
+    template_name = 'app/login.html'
+    
+# login handler
+def login_handler(request):
+    if request.user.is_authenticated:
+        print("already logged in")
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            print("logged in")
+            return redirect('home')
+        
+        else:
+            messages.info(request, "username or password is incorrect")
+            
+    return
+
+# registration handler
+def registration_handler(request):
+    if request.method == 'POST':
+        register_form = RegistrationForm(request.POST)
+        
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'account created')
+            print("account created")
+            
+            username = register_form.cleaned_data.get('username')
+            password = register_form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    return
