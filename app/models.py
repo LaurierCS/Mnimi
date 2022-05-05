5ab19e9344ae62469d1231b8c37628c958448037
Tony K <82545368+Nsiem@users.noreply.github.com>
2022-05-05 15:21:07 -0400

    Updated models, fixed variable names

--- app/models.py
+++ app/models.py
@@ -12,8 +12,8 @@ def getUserIDorCreate():
 # Decks class
 class Deck(models.Model):
     deckId = models.IntegerField(primary_key=True) # UUID
-    userId =  models.ForeignKey(User, on_delete=models.DO_NOTHING,  default=getUserIDorCreate)
-    creatorId = models.IntegerField()
+    user =  models.ForeignKey(User, on_delete=models.DO_NOTHING,  default=getUserIDorCreate)
+    creator = models.IntegerField()
     deckName = models.CharField(blank=False, max_length=50)
     dateCreated = models.DateField()
     isFavourite = models.BooleanField(default=False)
@@ -27,13 +27,13 @@ class Deck(models.Model):
 
     # returns QuerySet for all decks of User
     def getUsersDecks(user_ID):
-        decks = Deck.objects.filter(userId__id = user_ID)
+        decks = Deck.objects.filter(user__id = user_ID)
         return decks
 
     # returns single deck object
     def getDeck(deck_ID, user_ID):
         try:
-            deck = Deck.objects.get(deckId = deck_ID, userId__id = user_ID)
+            deck = Deck.objects.get(deckId = deck_ID, user__id = user_ID)
         except ObjectDoesNotExist:
             deck = False
         return deck
@@ -41,8 +41,8 @@ class Deck(models.Model):
 # Cards class
 class Card(models.Model):
     cardId = models.IntegerField(primary_key=True, unique=True)
-    deckId = models.ForeignKey(Deck, on_delete = models.DO_NOTHING)
-    userId =  models.ForeignKey(User, on_delete=models.DO_NOTHING,  default=getUserIDorCreate)
+    deck = models.ForeignKey(Deck, on_delete = models.DO_NOTHING)
+    user =  models.ForeignKey(User, on_delete=models.DO_NOTHING,  default=getUserIDorCreate)
     study_date = models.DateField(default = None)
     ELO = models.IntegerField(default= 0 )
     leech = models.IntegerField(default=0)
@@ -59,7 +59,7 @@ class Card(models.Model):
     # returns all of the cards in a specific users deck
     def getDecksCards(deck_ID, user_ID):
         try:
-            cards = Card.objects.filter(deckId = deck_ID, userId__id = user_ID)
+            cards = Card.objects.filter(deck = deck_ID, user__id = user_ID)
         except ObjectDoesNotExist:
             cards = False
         return cards
