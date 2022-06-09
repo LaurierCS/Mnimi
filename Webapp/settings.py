5682e4505dafedef330cf8644542892e305d1405
Jevin Sidhu <jevin.sidhu@gmail.com>
2022-06-09 16:55:30 -0400

    Update README.md, retrieve SECRET_KEY from .env

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o5ai32_u%y&n!(tbftje5r57_3+edq6iivyks-30(d&@n@nodl'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -81,6 +78,8 @@ WSGI_APPLICATION = 'Webapp.wsgi.application'
 env = environ.Env()
 # reading .env file
 environ.Env.read_env()
+
+SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
