62ec22e146eaeaa0e1ec8c31b9725484445e8d6a
Tony K <82545368+Nsiem@users.noreply.github.com>
2022-06-09 15:56:03 -0400

    ranking functionality is built

--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('', views.homepage, name='index'),
     path('deck/<int:deckId>/', views.deck, name='deck'),
     path('study/<int:deckId>/', views.study, name='study'),
-    path('update/<int:deckId>/<int:cardLedgerId>/<int:seconds>/', views.updateLedger, name='update' ),
+    path('update/<int:deckId>/<int:cardLedgerId>/<int:seconds>/<int:choice>/', views.updateLedger, name='update' ),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/create_account', views.create_account, name='create_account'),
     path('delete_card/<int:deckId>/<int:cardId>/', views.delete_card, name='deleteCard'),
     path('delete_deck/<int:deckId>/', views.delete_deck, name='deleteDeck'),
+    path('share/<int:deckId>/', views.share, name='share')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
